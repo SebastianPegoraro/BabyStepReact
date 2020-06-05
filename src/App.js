a0fd060c1456203a59197fa00ae77a5aabb7afb9
sebapego <sebapego18@gmail.com>
2020-06-05 19:34:42 -0300

    Class component and stateles function component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const App = () => <h1> Que ondis </h1>
+//Class component can hold states
+export class App extends Component {
+    render() {
+        return React.createElement('h1', null, 'Clase App')
+    }
+}
+
+//Stateless function
+// const App = () => <h1>Stateless</h1>
 
 export default App
