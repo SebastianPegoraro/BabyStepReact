004720ae9aaf86ce76ef815364ce4cff2eb501d4
sebapego <sebapego18@gmail.com>
2020-06-05 20:17:02 -0300

    Properties: PropTypes and defaultProps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class App extends Component {
     render() {
         return (
             <div>
                 <h1>Testing Render</h1>
-                <h3>Render using ()</h3>
+        <h3>{this.props.txt}</h3>
             </div>
         )
     }
+}
+
+App.propTypes = {
+    txt: PropTypes.string,
+    cat: PropTypes.number
+}
+
+App.defaultProps = {
+    txt: "This is default text"
 }
 
 export default App
