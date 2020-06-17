1a824f66f9e84127feb2779ec3fa3c3a29754df1
sebapego <sebapego18@gmail.com>
2020-06-17 18:23:46 -0300

    Component State

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            txt: 'state text',
+            cat: 0
+        }
+    }
+    update( e ){
+        this.setState({txt: e.target.value})
+    }
     render() {
         return (
             <div>
+                <input type="text" onChange={this.update.bind(this)} ></input>
                 <h1>Testing Render</h1>
         <h3>{this.props.txt}</h3>
+        <h3>{this.state.txt} - {this.state.cat}</h3>
             </div>
         )
     }
