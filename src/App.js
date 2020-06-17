bb954aa7dd35aab133d34d3fa62a6ecbc0ac659b
sebapego <sebapego18@gmail.com>
2020-06-17 19:31:43 -0300

    Nested Data with Reacts props.children

--- src/App.js
+++ src/App.js
@@ -18,15 +18,21 @@ export class App extends Component {
                 <Widget update={this.update.bind(this)}></Widget>
                 <input type="text" onChange={this.update.bind(this)} ></input>
                 <h1>Testing Render</h1>
-        <h3>{this.props.txt}</h3>
-        <h3>{this.state.txt} - {this.state.cat}</h3>
+                <h3>{this.props.txt}</h3>
+                <h3>{this.state.txt} - {this.state.cat}</h3>
+
+                <div>
+                    <Button>I <Heart></Heart> Random</Button>
+                </div>
             </div>
         )
     }
 }
 //Stateless function
-const Widget = (props) => 
-<input type="text" onChange={props.update} ></input>
+const Widget = (props) => <input type="text" onChange={props.update} ></input>
+
+//Stateless function
+const Button = (props) => <button>{props.children}</button>
 
 App.propTypes = {
     txt: PropTypes.string,
@@ -35,6 +41,14 @@ App.propTypes = {
 
 App.defaultProps = {
     txt: "This is default text"
+}
+
+class Heart extends Component{
+    render(){
+        return (
+            <span>&hearts;</span>
+        )
+    }
 }
 
 export default App
