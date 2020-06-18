93b5a740a23592d9bbd0b480d8ddfef71b82e91a
sebapego <sebapego18@gmail.com>
2020-06-17 22:06:43 -0300

    React Reference to a specific Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 
 export class App extends Component {
     constructor(){
@@ -7,14 +8,17 @@ export class App extends Component {
         this.state = {
             txt: 'state text',
             cat: 0,
-            currentEvent: '----'
+            currentEvent: '----',
+            a:''
         }
         this.update = this.update.bind(this)
     }
     update( e ){
         this.setState({
             txt: e.target.value,
-            currentEvent: e.type  //React Synthetic Event System
+            currentEvent: e.type, //React Synthetic Event System
+            a: this.a.refs.input.value,
+            b: this.refs.b.value 
         })
     }
     render() {
@@ -45,6 +49,19 @@ export class App extends Component {
 
                     <h1>{this.state.currentEvent}</h1>
                 </div>
+
+                <div>
+                    <Input 
+                        ref={component => this.a = component}
+                        update={this.update.bind(this)}
+                    /> {this.state.a}
+                    <hr />
+                    <input 
+                        ref="b"
+                        type="text"
+                        onChange={this.update.bind(this)}
+                    /> {this.state.b}
+                </div>
             </div>
         )
     }
@@ -68,6 +85,14 @@ class Heart extends Component{
     render(){
         return (
             <span>&hearts;</span>
+        )
+    }
+}
+
+class Input extends Component{
+    render(){
+        return(
+            <input ref="input" type="text" onChange={this.props.update}></input>
         )
     }
 }
